d5b56fe78d982e90fde0ba85b2cb252e8c85464e
Pettson <m.k.pettersson@gmail.com>
2014-12-11 12:14:14 +0100

    Update

--- SimulatedWater/src/data_types/ParticleInterface.java
+++
@@ -1,9 +0,0 @@
-package data_types;
-
-import org.lwjgl.util.vector.Vector3f;
-
-public interface ParticleInterface {
-	public float getDensity();
-	public float getPressure();
-	public Vector3f getPosition();
-}
